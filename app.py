231a3d24823186e1d7867426fd3e2d999534775f
Charles Hayes <cfhayes16@gmail.com>
2022-07-18 05:53:10 -0600

    Created endpoint to get a project by id

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import re
 import shutil
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, url_for
 
 app = Flask(__name__)
 
@@ -30,6 +30,73 @@ def create_project_endpoint():
         response.status_code = 201
 
     return response
+
+
+@app.route('/api/projects/<project_id>')
+def get_project_endpoint(project_id):
+    try:
+        if project_exists(project_id) is False:
+            body = {
+                'meta_data': {
+                    'message': "project_id '{}' not found".format(project_id)
+                }
+            }
+            resp = jsonify(body)
+            resp.status_code = 404
+            return resp
+
+        project_path = get_project_path(project_id)
+        project_reports_path = f'{project_path}/reports'
+
+        reports_entity = []
+
+        for file in os.listdir(project_reports_path):
+            file_path = f'{project_reports_path}/{file}/index.html'
+            is_file = os.path.isfile(file_path)
+            if is_file is True:
+                report = url_for('get_reports_endpoint', project_id=project_id,
+                                 path='{file}/index.html', _external=True)
+                reports_entity.append([report, os.path.getmtime(file_path), file])
+        reports_entity.sort(key=lambda reports_entity: reports_entity[1], reverse=True)
+        reports = []
+        reports_id = []
+        latest_report = None
+
+        for report_entity in reports_entity:
+            link = report_entity[0]
+            if report_entity[2].lower() != 'latest':
+                reports.append(link)
+                reports_id.append(report_entity[2])
+            else:
+                latest_report = link
+
+        if latest_report is not None:
+            reports.insert(0, latest_report)
+            reports_id.insert(0, 'latest')
+        body = {
+            'data': {
+                'project': {
+                    'id': project_id,
+                    'reports': reports,
+                    'reports_id': reports_id
+                },
+            },
+            'meta_data': {
+                'message': "Project successfully obtained"
+                }
+            }
+        resp = jsonify(body)
+        resp.status_code = 200
+        return resp
+    except Exception as ex:
+        body = {
+            'meta_data': {
+                'message': str(ex)
+            }
+        }
+        resp = jsonify(body)
+        resp.status_code = 400
+        return resp
 
 
 @app.route('/api/projects/<project_id>', methods=['DELETE'])
